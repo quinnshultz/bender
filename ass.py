56def385d489517c57efcef0f2bbd59589a4de3d
Quinn Shultz <vanoxite@gmail.com>
2023-07-21 20:47:41 -0600

    Greatly improved prompt. Started implementing IRL functionality.

---
+++ ass.py
@@ -0,0 +1,7 @@
+# For interacting with the Robot's hardware
+class ShinyMetal:
+    def bend():
+        print("Bending girder.")
+
+    def move_forward(distance):
+        print("Moving " + distance + " feet.")
